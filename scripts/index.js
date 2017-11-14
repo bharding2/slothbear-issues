ddda038434850508a4c8a1714363f67edbab95d7
bharding2 <b.harding@gmail.com>
2017-11-14 10:23:06 -0800

    selfimposed linecount refactor

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,6 @@
 const slothbearIssues = angular.module('slothbearIssues', []);
 
 slothbearIssues.controller('SlothbearIssuesController', ['$http', function($http) {
-  this.issues = [];
-
   $http.get('https://api.github.com/repos/bharding2/slothbear-issues/issues')
-    .then((issues) => {
-      this.issues = issues.data;
-    });
+    .then(issues => this.issues = issues.data);
 }]);
