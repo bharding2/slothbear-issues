b59a84055993fd4aa5c4e111088afd49e3f32db3
bharding2 <b.harding@gmail.com>
2017-11-14 10:17:15 -0800

    getting issues

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,10 @@
 const slothbearIssues = angular.module('slothbearIssues', []);
 
 slothbearIssues.controller('SlothbearIssuesController', ['$http', function($http) {
-  this.stuff = 'stuff';
+  this.issues = [];
+
+  $http.get('https://api.github.com/repos/bharding2/slothbear-issues/issues')
+    .then((issues) => {
+      this.issues = issues.data;
+    });
 }]);
